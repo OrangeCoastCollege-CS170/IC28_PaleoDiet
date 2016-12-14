46cb7e0aaa614471784acde7e290d11418cbd721
Badajoz <sbadajoz@student.cccd.edu>
2016-12-14 11:41:10 -0800

    Add required functionality

--- src/edu/orangecoastcollege/sbadajoz/ic28/PaleoFood.java
+++ src/edu/orangecoastcollege/sbadajoz/ic28/PaleoFood.java
@@ -1,5 +1,21 @@
 package edu.orangecoastcollege.sbadajoz.ic28;
 
 public abstract class PaleoFood {
-
+	protected String mName;
+	protected int mNumCalories;
+	
+	public String getName() {
+		return mName;
+	}
+	public int getCalories() {
+		return mNumCalories;
+	}
+	public void setName(String name) {
+		this.mName = name;
+	}
+	public void setCalories(int calories) {
+		this.mNumCalories = calories;
+	}
+	
+	
 }
