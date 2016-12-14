ab884e5afebdb800f440f2aa07aea5497f509a80
Badajoz <sbadajoz@student.cccd.edu>
2016-12-14 11:54:31 -0800

    Add vaiable, numCarbohydrates

--- src/edu/orangecoastcollege/sbadajoz/ic28/PaleoFood.java
+++ src/edu/orangecoastcollege/sbadajoz/ic28/PaleoFood.java
@@ -3,6 +3,7 @@ package edu.orangecoastcollege.sbadajoz.ic28;
 public abstract class PaleoFood {
 	protected String mName;
 	protected int mNumCalories;
+	protected int mNumCarbohydrates;
 	
 	public String getName() {
 		return mName;
@@ -10,11 +11,17 @@ public abstract class PaleoFood {
 	public int getCalories() {
 		return mNumCalories;
 	}
+	public int getCarbohydrates() {
+		return mNumCarbohydrates;
+	}
 	public void setName(String name) {
 		this.mName = name;
 	}
 	public void setCalories(int calories) {
 		this.mNumCalories = calories;
+	}
+	public void setCarbohydrates(int carbohydrates) {
+		this.mNumCarbohydrates = carbohydrates;
 	}
 	
 	
