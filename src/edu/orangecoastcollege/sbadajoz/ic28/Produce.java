1de0eb342cbc723c74b0874215c769f40cce9648
Badajoz <sbadajoz@student.cccd.edu>
2016-12-14 12:12:01 -0800

    Complete required functionality

--- src/edu/orangecoastcollege/sbadajoz/ic28/Produce.java
+++ src/edu/orangecoastcollege/sbadajoz/ic28/Produce.java
@@ -3,5 +3,56 @@ package edu.orangecoastcollege.sbadajoz.ic28;
 import java.io.Serializable;
 
 public class Produce extends PaleoFood implements Serializable {
+	private boolean mOrganic;
+	
+	 public Produce(String name, int calories, int carbohydrates, boolean organic) {
+		 mName = name;
+		 mNumCalories = calories;
+		 mNumCarbohydrates = carbohydrates;
+		 mOrganic = organic;
+	 }
 
+	public boolean ismOrganic() {
+		return mOrganic;
+	}
+
+	public void setmOrganic(boolean mOrganic) {
+		this.mOrganic = mOrganic;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (mOrganic ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produce other = (Produce) obj;
+		if (mName != other.mName)
+			return false;
+		if (mNumCalories != other.mNumCalories)
+			return false;
+		if (mNumCarbohydrates != other.mNumCarbohydrates)
+			return false;
+		if (mOrganic != other.mOrganic)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return ((mOrganic)? "Organic":"") + "Produce: " + mName + "," + mNumCalories + "calories, "
+				+ mNumCarbohydrates + "g carbs";
+	}
+	 
+	 
 }
